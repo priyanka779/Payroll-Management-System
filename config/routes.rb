ae278d39be1e00a49a6a6986033e8a71b9778069
Anand <Anandpandey0808@gmail.com>
2022-07-18 18:50:54 +0530

    create leavemanagemet and create form also

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+
+  resources :leave_managements
   get 'departments/index'
   resources :employees
   devise_for :users
---
+++ db/migrate/20220718123737_create_leave_managements.rb
@@ -0,0 +1,12 @@
+class CreateLeaveManagements < ActiveRecord::Migration[6.1]
+  def change
+    create_table :leave_managements do |t|
+      t.string :leave_type
+      t.integer :leave_unit
+      t.string  :reason
+      t.date :start_date
+      t.date :end_date
+      t.timestamps
+    end
+  end
+end
