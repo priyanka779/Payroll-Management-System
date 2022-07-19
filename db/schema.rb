550eb735ea9aff75f13491fc1b19c616e3c21261
Anand <Anandpandey0808@gmail.com>
2022-07-19 18:45:31 +0530

    Working on Leavemanagement model and department

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_18_123737) do
+ActiveRecord::Schema.define(version: 2022_07_19_054648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2022_07_18_123737) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_leave_managements_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
